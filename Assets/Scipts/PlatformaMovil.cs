b7c37faefef95c04fdc1a938f6d4015ef98c807d
OSCARGT94 <oscarfernan94@gmail.com>
2024-10-02 18:09:03 +0200

    Suelo movedizo por timer

--- Assets/Scipts/PlatformaMovil.cs
+++ Assets/Scipts/PlatformaMovil.cs
@@ -6,7 +6,10 @@ public class PlatformaMovil : MonoBehaviour
 {
     [SerializeField] Vector3 movimiento;
     [SerializeField] float velocidad;
+    [SerializeField] float timer, restTimer;
     bool sentidoGiro = true;
+
+    float tiempo;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +19,51 @@ public class PlatformaMovil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (sentidoGiro)
+
+        movimientoHorizontal();
+
+    }
+    void movimientoHorizontal()
+    {
+        if (timer <= -restTimer)
+        {
+            timer = restTimer;
+        }
+
+        timer -= Time.deltaTime;
+        if (timer > 0)
         {
             transform.Translate(movimiento.normalized * velocidad * Time.deltaTime);
-            if (transform.position.z <= -15)
-            {
-                sentidoGiro = false;
-            }
+            
         }
-        else if (!sentidoGiro)
+        else if (timer <= 0)
         {
             transform.Translate(movimiento.normalized * -velocidad * Time.deltaTime);
-            if (transform.position.z >= 15)
-            {
-                sentidoGiro=true;
-            }
+
         }
+
+        
+
+
+
+
+
+        //if (sentidoGiro)
+        //{
+        //    transform.Translate(movimiento.normalized * velocidad * Time.deltaTime);
+        //    if (transform.position.z <= -15)
+        //    {
+        //        sentidoGiro = false;
+        //    }
+        //}
+        //else if (!sentidoGiro)
+        //{
+        //    transform.Translate(movimiento.normalized * -velocidad * Time.deltaTime);
+        //    if (transform.position.z >= 15)
+        //    {
+        //        sentidoGiro=true;
+        //    }
+        //}
+
     }
 }
