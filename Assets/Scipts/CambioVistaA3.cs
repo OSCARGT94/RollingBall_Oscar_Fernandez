b6c8cb65da66f55ef747db3023af728b62eeb363
Oscar <oscarfernan94@gmail.com>
2024-11-06 10:34:22 +0100

    Arreglo sonido inicial y cambios de camara

--- Assets/Scipts/CambioVistaA3.cs
+++ Assets/Scipts/CambioVistaA3.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CambioVistas : MonoBehaviour
+public class CambioVistaA3 : MonoBehaviour
 {
     [SerializeField] GameObject camara1, camaCenital;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            camara1.SetActive(false);
 
-            camaCenital.SetActive(true);
+            camara1.SetActive(true);
+
+            camaCenital.SetActive(false);
 
         }
     }
