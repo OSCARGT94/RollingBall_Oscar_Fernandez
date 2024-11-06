e2b18adf5e8b779cb6322982f847ce9120572283
Oscar <oscarfernan94@gmail.com>
2024-11-06 13:45:51 +0100

    MINIMAPA LABERINTO

--- Assets/Scipts/TextosFlotantes.cs
+++ Assets/Scipts/TextosFlotantes.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class TextosFlotantes : MonoBehaviour
 {
     [SerializeField] GameObject textoLaberinto;
+    [SerializeField] GameObject minimap;
     // Start is called before the first frame update
     void Start()
     {
         textoLaberinto.SetActive(false);
+        minimap.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,6 +30,20 @@ public class TextosFlotantes : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             textoLaberinto.SetActive(false);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.K))
+            {
+                minimap.SetActive(true);
+            }
+            if (Input.GetKey(KeyCode.U))
+            {
+                minimap.SetActive(false);
+            }
         }
     }
 }
